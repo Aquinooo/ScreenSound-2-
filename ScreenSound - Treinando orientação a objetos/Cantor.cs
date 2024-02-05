5c7616adb1e95fc3ac935db8a47dd4e04b0ceec0
Danilo Aquino <danilo.aquiino@outlook.com>
2024-02-05 15:32:08 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound - Treinando orientação a objetos/Cantor.cs
@@ -0,0 +1,25 @@
+﻿class Cantor
+{
+    private List<Album> albums = new List<Album>();
+
+    public Cantor(string nome)
+    {
+        Nome = nome;
+    }
+
+    public string Nome { get; }
+
+    public void AdicionarAlbum(Album album)
+    {
+        albums.Add(album);
+    }
+
+    public void ExibirDiscografia()
+    {
+        Console.WriteLine($"Discografia do artista {Nome}");
+        foreach (Album album in albums)
+        {
+            Console.WriteLine($"Álbum: {album.Nome} ({album.DuracaoTotal})");
+        }
+    }
+}
---
+++ ScreenSound - Treinando orientação a objetos/Program.cs
@@ -0,0 +1,27 @@
+﻿Cantor veigh = new Cantor("Veigh");
+
+Album albumDoVeigh = new Album("Dos prédios");
+
+Musica musica1 = new Musica(veigh, "Novo balanço")
+{
+    Duracao = 213,
+    Disponivel = true,
+};
+
+Musica musica2 = new Musica(veigh, "Foto da unha")
+{
+    Duracao = 354,
+    Disponivel = false,
+};
+
+albumDoVeigh.AdicionarMusica(musica1);
+albumDoVeigh.AdicionarMusica(musica2);
+veigh.AdicionarAlbum(albumDoVeigh);
+
+musica1.ExibirFichaTecnica();
+Console.WriteLine("*******************");
+musica2.ExibirFichaTecnica();
+Console.WriteLine("*******************");
+albumDoVeigh.ExibirMusicasDoAlbum();
+veigh.ExibirDiscografia();
+
